31b7bb77627d871e78135449fc559be7c627731b
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2025-04-14 18:29:21 +0530

    feature: added whatsapp sms service for notification

--- backend/notification-service/src/controllers/notification.controller.js
+++ backend/notification-service/src/controllers/notification.controller.js
@@ -1,4 +1,4 @@
-import { processEmail } from '../services/notification.service.js';
+import { processEmail, processSMS, processWhatsApp } from '../services/notification.service.js';
 import validateNotification from '../validations/notification.validator.js';
 
 export async function sendEmailNotification(req, res) {
@@ -13,5 +13,31 @@ export async function sendEmailNotification(req, res) {
     res.status(200).json({ message: 'Notification sent', info });
   } catch (error) {
     res.status(500).json({ message: 'Failed to send notification', error: error.message });
+  }
+}
+
+export async function sendSMSNotification(req, res) {
+  const { phone, message } = req.body;
+
+  if (!phone || !message) {
+    return res.status(400).json({ error: 'Phone and message are required' });
+  }
+
+  try {
+    const info = await processSMS({ phone, message });
+    res.status(200).json({ message: 'SMS sent', info });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to send SMS', error: error.message });
+  }
+}
+
+export async function sendWhatsAppNotification(req, res) {
+  const { phone, template, params } = req.body;
+
+  try {
+    const info = await processWhatsApp({ phone, template, params  });
+    res.status(200).json({ message: 'WhatsApp message sent', info });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to send WhatsApp message', error: error.message });
   }
 }
