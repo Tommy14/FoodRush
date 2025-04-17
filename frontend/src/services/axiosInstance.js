cbb16835f764e51bf864be65aa2e5b8ffd0f0fc5
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2025-04-18 03:33:03 +0530

    chore: Update user token in axiosInstance.js

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 // src/services/axiosInstance.js
 import axios from 'axios';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2ZlYTJkZTY5MzMzMWQwNWExNDQzN2QiLCJyb2xlIjoiZGVsaXZlcnlfcGVyc29uIiwiZW1haWwiOiJkZWxpdmVyeUBnbWFpbC5jb20iLCJpYXQiOjE3NDQ3NDQ4MzIsImV4cCI6MTc0NDc0ODQzMn0.kYxLhINDxVTGkgcbn0F7Pthoh9rl5_tS0oGLLWUJPlE';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2ZlMmRlYTdmMTc0YzlhOWFlNDkyYjYiLCJyb2xlIjoiY3VzdG9tZXIiLCJlbWFpbCI6InRoaWhhbnNpZ0BnbWFpbC5jb20iLCJpYXQiOjE3NDQ5MTQyOTcsImV4cCI6MTc0NDkxNzg5N30.wJoxmRJz2zvr8vFWyYOL9I5X_qNAlgOXR7VRHuhWYrU';
 
 const instance = axios.create({
     baseURL: 'http://localhost:5001',
