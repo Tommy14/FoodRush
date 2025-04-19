5f0e6c26ec9f36d859dd84d87959a37b2fbed2c4
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2025-04-20 02:02:30 +0530

    Refactor RESTAURANT_SERVIC_URL to RESTAURANT_SERVICE_URL

--- backend/delivery-service/src/config/index.js
+++ backend/delivery-service/src/config/index.js
@@ -7,4 +7,4 @@ export const INTERNAL_SERVICE_API_KEY = process.env.INTERNAL_SERVICE_API_KEY;
 export const ORDER_SERVICE_URL = process.env.ORDER_SERVICE_URL;
 export const SYSTEM_JWT = process.env.SYSTEM_JWT;
 export const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
-export const RESTAURANT_SERVIC_URL = process.env.RESTAURANT_SERVICE_URL;
+export const RESTAURANT_SERVICE_URL = process.env.RESTAURANT_SERVICE_URL;
