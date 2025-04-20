f9401d90f35961c66b4e05e98971fa35079700e3
thihansi <thihansig@gmail.com>
2025-04-21 01:28:27 +0530

    feat: Refactor route imports in app.js and enhance restaurant and location routes with new endpoints and authentication

--- bff/app.js
+++ bff/app.js
@@ -4,6 +4,14 @@ import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.routes.js';
+import restaurantRoutes from './routes/restaurant.routes.js';
+import orderRoutes from './routes/order.routes.js';
+import deliveryRoutes from './routes/delivery.routes.js';
+import paymentRoutes from './routes/payment.routes.js';
+import menuRoutes from './routes/menu.route.js';
+import reviewRoutes from './routes/review.routes.js';
+import locationRoutes from './routes/location.routes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,15 +33,6 @@ const uploadDir = path.join(__dirname, 'tmp', 'uploads');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
-// Import Routes
-import authRoutes from './routes/auth.routes.js';
-import restaurantRoutes from './routes/restaurant.routes.js';
-import orderRoutes from './routes/order.routes.js';
-import deliveryRoutes from './routes/delivery.routes.js';
-import paymentRoutes from './routes/payment.routes.js';
-import menuRoutes from './routes/menu.route.js';
-import reviewRoutes from './routes/review.routes.js';
-import locationRoutes from './routes/location.routes.js';
 
 app.use('/bff/auth', authRoutes);
 app.use('/bff/restaurants', restaurantRoutes);
