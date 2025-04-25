c7e51ff9105f61586ff29b1d7cb1ae1608f73eff
Thamindu Vimansha <159893134+Tommy14@users.noreply.github.com>
2025-04-25 20:44:59 +0530

    feat: Update payment service port configuration
    - Updated the `PORT` configuration in the payment service to use the `PAYMENT_PORT` environment variable instead of the default value. This allows for more flexibility in configuring the payment service port.
    
    chore: Update secrets for delivery, order, and user services
    
    - Added the `SYSTEM_JWT` and `ORDER_SERVICE_URL` environment variables to the `delivery-secret.yaml` and `order-secret.yaml` files respectively. These variables are used for authentication and communication with other services.
    - Added the `USER_SERVICE_URL` and `INTERNAL_SERVICE_API_KEY` environment variables to the `delivery-secret.yaml` and `order-secret.yaml` files respectively. These variables are used for communication with the user service and internal service.
    - Added the `NOTIFICATION_SERVICE_URL` environment variable to the `order-secret.yaml` file. This variable is used for communication with the notification service.
    - Added the `DELIVERY_SERVICE_URL` environment variable to the `order-secret.yaml` file. This variable is used for communication with the delivery service.
    
    chore: Update secrets for restaurant service
    
    - Added the `MONGO_URI_RESTAURANT_SERVICE` environment variable to the `restaurant-secret.yaml` file. This variable is used for connecting to the restaurant database.
    
    chore: Remove unused location and payment secrets
    
    - Removed the `location-secret.yaml` and `payment-secret.yaml` files as they are no longer needed.

--- backend/payment-service/server.js
+++ backend/payment-service/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 9400;
+const PORT = process.env.PAYMENT_PORT || 9400;
 
 app.listen(PORT, () => {
   console.log(`Payment Service is running on port ${PORT}`);
